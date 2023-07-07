8677c17919467189b5af4784e97f08bee2325525
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-07-08 07:57:20 +0900

    サイドバーコンポーネント作成

---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Sidebar.scss"
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <div className='sidebarLeft'></div>
+      <div className='sidebarRight'></div>
+    </div>
+  )
+}
+
+export default Sidebar
