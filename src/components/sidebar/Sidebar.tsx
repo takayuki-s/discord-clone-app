eab74a4ee539fe9c4a6fceb725d6c10393efa228
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-07-09 14:32:21 +0900

    サイドバースタイリング

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -4,8 +4,19 @@ import "./Sidebar.scss"
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-      <div className='sidebarLeft'></div>
-      <div className='sidebarRight'></div>
+      <div className='sidebarLeft'>
+        <div className='serverIcon'>
+          <img src='./logo192.png' />
+        </div>
+        <div className='serverIcon'>
+          <img src='./logo192.png' />
+        </div>
+      </div>
+      <div className='sidebarRight'>
+        <div className='sidbarTop'>
+          <h3>Discord Clone</h3>
+        </div>
+      </div>
     </div>
   )
 }
