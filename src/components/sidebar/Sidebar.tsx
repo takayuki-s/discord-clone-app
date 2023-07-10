e58eb21dfc8e0a439f8abd04fbe62ce8dc0f189e
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-07-11 07:37:12 +0900

    サイドバーにアイコン追加、CSS調整

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Sidebar.scss'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 
 const Sidebar = () => {
   return (
@@ -13,8 +14,9 @@ const Sidebar = () => {
         </div>
       </div>
       <div className="sidebarRight">
-        <div className="sidbarTop">
+        <div className="sidebarTop">
           <h3>Discord Clone</h3>
+          <ExpandMoreIcon />
         </div>
       </div>
     </div>
