31a4d2b99ecbfa8e3f991990c009367c88b6bc90
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-07-19 07:12:52 +0900

    チャットヘッダー作成

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './Chat.scss'
+import ChatHeader from './ChatHeader'
 
 const Chat = () => {
   return (
     <div className="chat">
       {/* chatHeader */}
+      <ChatHeader />
       {/* chatMessage */}
+      <div className="chatMessage"></div>
       {/* chatInput */}
+      <div className="chatInput"></div>
     </div>
   )
 }
