a810d56f09c859b16008ae7c0e2083ed810443f9
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-07-21 08:23:46 +0900

    Update Chat.tsx

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -5,11 +5,8 @@ import ChatHeader from './ChatHeader'
 const Chat = () => {
   return (
     <div className="chat">
-      {/* chatHeader */}
       <ChatHeader />
-      {/* chatMessage */}
       <div className="chatMessage"></div>
-      {/* chatInput */}
       <div className="chatInput"></div>
     </div>
   )
