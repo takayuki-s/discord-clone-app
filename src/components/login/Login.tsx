1061d14a96c659f301ec2a60357ec5db1a6c54c0
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-08-01 09:24:38 +0900

    Googleログイン

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import './Login.scss'
 import { Button } from '@mui/material'
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import { auth, provider } from '../../firebase'
 
 const Login = () => {
+  const signIn = () => {
+    signInWithPopup(auth, provider).catch((error) => {
+      console.log(error)
+    })
+  }
   return (
     <div className="login">
       <div className="loginLogo">
         <img src="./discordIcon.png" />
       </div>
 
-      <Button>ログイン</Button>
+      <Button onClick={signIn}>ログイン</Button>
     </div>
   )
 }
