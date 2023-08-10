4f47d4262f2db66f2a50c03e8708f561dc9957ea
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-08-11 08:59:49 +0900

    チャンネル情報をReduxで管理

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -6,8 +6,10 @@ import CardGiftcardIcon from '@mui/icons-material/CardGiftcard'
 import GifIcon from '@mui/icons-material/Gif'
 import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions'
 import ChatMessage from './ChatMessage'
+import { useAppSelector } from '../../app/hooks'
 
 const Chat = () => {
+  const channelName = useAppSelector((state) => state.channel)
   return (
     <div className="chat">
       <ChatHeader />
