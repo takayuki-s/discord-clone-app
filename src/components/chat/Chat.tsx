71152c3006bf64f31473b17d5ee8e97ecd413534
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-08-14 08:07:46 +0900

    サブコレクションにメッセージ情報を追加

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -7,13 +7,43 @@ import GifIcon from '@mui/icons-material/Gif'
 import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions'
 import ChatMessage from './ChatMessage'
 import { useAppSelector } from '../../app/hooks'
+import {
+  CollectionReference,
+  DocumentData,
+  DocumentReference,
+  addDoc,
+  collection,
+  serverTimestamp,
+} from 'firebase/firestore'
+import { db } from '../../firebase'
 
 const Chat = () => {
   const [inputText, setInputText] = useState<string>('')
+  const channelId = useAppSelector((state) => state.channel.channelId)
+  const user = useAppSelector((state) => state.user.user)
 
-  const sendMessage = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const sendMessage = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
     e.preventDefault()
-    console.log(e)
+
+    // channelコレクションの中にあるmessageコレクションの中にメッセージ情報を入れる
+    const collectionRef: CollectionReference<DocumentData> = collection(
+      db,
+      'channels',
+      String(channelId),
+      'message',
+    )
+
+    const docRef: DocumentReference<DocumentData> = await addDoc(
+      collectionRef,
+      {
+        message: inputText,
+        timestamp: serverTimestamp,
+        user: user,
+      },
+    )
+    console.log(docRef)
   }
 
   const channelName = useAppSelector((state) => state.channel.channelName)
